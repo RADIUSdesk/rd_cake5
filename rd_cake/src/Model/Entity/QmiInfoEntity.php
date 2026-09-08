0e9fdbd74a49f6c9175b567aaf6dce6cc039488f
Dirk van der Walt <dirkvanderwalt@gmail.com>
2026-09-08 15:49:44 +0000

    Fix migration bug 1

--- rd_cake/src/Model/Entity/QmiInfoEntity.php
+++ rd_cake/src/Model/Entity/QmiInfoEntity.php
@@ -1,14 +1,26 @@
-<?php 
+<?php
+declare(strict_types=1);
 
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
 
-class QmiInfoEntity extends Entity{
-
-    // Define accessible fields if needed
-    protected $_accessible = [
-        '*' => true,  // Allow all fields to be mass assigned
+/**
+ * QmiInfo Entity
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Cake\I18n\DateTime $created
+ * @property \Cake\I18n\DateTime $modified
+ */
+class QmiInfoEntity extends Entity
+{
+    /**
+     * Fields that can be mass assigned
+     *
+     * @var array<string, bool>
+     */
+    protected array $_accessible = [
+        '*' => true,
     ];
 }
-
